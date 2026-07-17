49140d39febd93371148c31a8635f8f26425c285
nielsleysen-web <nielsleysen@gmail.com>
2026-07-17 16:06:31 +0200

    in-production.js

---
+++ pages/in-production.js
@@ -0,0 +1,6 @@
+// pages/in-production.js
+import MarketingBoard from "../components/MarketingBoard";
+
+export default function InProduction() {
+  return <MarketingBoard status="In production" title="In Production" icon="🎨" />;
+}
