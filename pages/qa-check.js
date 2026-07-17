7be32034569a4ab88fd386902fcc48d2ab4322bf
nielsleysen-web <nielsleysen@gmail.com>
2026-07-17 16:06:58 +0200

    qa-check.js

---
+++ pages/qa-check.js
@@ -0,0 +1,6 @@
+// pages/qa-check.js
+import MarketingBoard from "../components/MarketingBoard";
+
+export default function QACheck() {
+  return <MarketingBoard status="QA Check" title="QA Check" icon="✅" showHidden />;
+}
