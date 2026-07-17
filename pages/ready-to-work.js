39ff3e8852231ad1614928b32668341c5507b3f3
nielsleysen-web <nielsleysen@gmail.com>
2026-07-17 16:05:57 +0200

    ready-to-work.js

---
+++ pages/ready-to-work.js
@@ -0,0 +1,6 @@
+// pages/ready-to-work.js
+import MarketingBoard from "../components/MarketingBoard";
+
+export default function ReadyToWork() {
+  return <MarketingBoard status="Ready to work" title="Ready To Work" icon="🎬" />;
+}
