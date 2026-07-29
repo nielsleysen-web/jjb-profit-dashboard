534fe0065b9236e15fc99e29ace463d81fa3221f
nielsleysen-web <nielsleysen@gmail.com>
2026-07-29 09:14:01 +0200

    Delete pages/in-production.js

--- pages/in-production.js
+++
@@ -1,6 +0,0 @@
-// pages/in-production.js
-import MarketingBoard from "../components/MarketingBoard";
-
-export default function InProduction() {
-  return <MarketingBoard status="In production" title="In Production" icon="🎨" />;
-}
