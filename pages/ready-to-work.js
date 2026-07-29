b7c1344f3411311bdda3aa174895f4f21126ae9b
nielsleysen-web <nielsleysen@gmail.com>
2026-07-29 09:13:47 +0200

    Delete pages/ready-to-work.js

--- pages/ready-to-work.js
+++
@@ -1,6 +0,0 @@
-// pages/ready-to-work.js
-import MarketingBoard from "../components/MarketingBoard";
-
-export default function ReadyToWork() {
-  return <MarketingBoard status="Ready to work" title="Ready To Work" icon="🎬" />;
-}
