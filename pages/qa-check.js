c9277ee99b81bdf35da6b10c6838958471eeec97
nielsleysen-web <nielsleysen@gmail.com>
2026-07-29 09:14:13 +0200

    Delete pages/qa-check.js

--- pages/qa-check.js
+++
@@ -1,6 +0,0 @@
-// pages/qa-check.js
-import MarketingBoard from "../components/MarketingBoard";
-
-export default function QACheck() {
-  return <MarketingBoard status="QA Check" title="QA Check" icon="✅" showHidden />;
-}
